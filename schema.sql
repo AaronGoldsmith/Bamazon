f46c0882999f4f05e9a5394e6dac2d6b25021de5
Aaron Goldsmith <agoldsmith@scu.edu>
2018-09-02 02:16:22 -0700

    fixing schema to match required

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 DROP DATABASE IF EXISTS bamazon_DB;
 CREATE DATABASE bamazon_DB;
 
-
-CREATE TABLE inventory(
-   id INT NOT NULL AUTO_INCREMENT,
-   item VARCHAR(100) NOT NULL,
+USE bamazon_DB;
+CREATE TABLE products(
+   item_id INT NOT NULL AUTO_INCREMENT,
+   product_name VARCHAR(100) NOT NULL,
+   department_name VARCHAR(50) NOT NULL,
    price DECIMAL(10,2) NOT NULL,
-   stock INT DEFAULT 0,
-   PRIMARY KEY (id)
+   stock_quantity INT DEFAULT 0,
+   PRIMARY KEY (item_id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,18 +1,35 @@
 USE bamazon_DB;
 
-INSERT INTO inventory (item,price,stock) 
-VALUES ("item1",10,100);
+INSERT INTO products(product_name,department_name,price) 
+VALUES ("Cat bed","pet supplies", 100);
 
-INSERT INTO inventory (item,price,stock) 
-VALUES ("item2",19.50,100);
+INSERT INTO products(product_name,department_name,price)
+VALUES ("canned soup","groceries",2.0);
 
-INSERT INTO inventory (item,price,stock) 
-VALUES ("item3",19.50,100);
+INSERT INTO products(product_name,department_name,price)
+VALUES ("cat carrier","travel",29.50);
 
-INSERT INTO inventory (item,price,stock) 
-VALUES ("item4",19.50,100);
+INSERT INTO products(product_name,department_name,price)
+VALUES ("cat tree","pet supplies",19.50);
 
-INSERT INTO inventory (item,price,stock) 
-VALUES ("pillow5",19.50,100);
+INSERT INTO products(product_name,department_name,price)
+VALUES ("dog treats","pet supplies",10.50);
 
-SELECT * FROM inventory;
+INSERT INTO products(product_name,department_name,price)
+VALUES ("cereal","groceries",4.00);
+
+INSERT INTO products(product_name,department_name,price)
+VALUES ("spiral notebook","office supplies",19.50);
+
+INSERT INTO products(product_name,department_name,price)
+VALUES ("USA flag","home decor",12.00);
+
+INSERT INTO products(product_name,department_name,price)
+VALUES ("flashlight","camping/outdoors",19.50);
+
+INSERT INTO products(product_name,department_name,price)
+VALUES ("desk organizer","office supplies",3.50);
+
+
+
+SELECT * FROM products;
